e0952de7a237df3416b39f076344f9f0d7231ebf
Edin Kucevic <edin@Edins-MacBook-Air.local>
2022-05-06 18:48:03 +0200

    it camp

---
+++ uvod/JS/app.js
@@ -0,0 +1,28 @@
+//alert ("radi");
+
+console.log ("Prvi ispis u JS konzoli");
+
+'nesto' //string
+65 //number
+true,false //boolean
+['string',50,true,["string",19]]; //boolean
+
+null //ne postoji informacija
+undefined //podatak je tu ali nedefinisan
+{}
+// tipovi podataka mogu biti primitivni i ne primitivni..
+
+
+// memorijske lokacije se oznacavaju heksadecimalnim brojevima,na njima se nalaze kutije sa informacijama
+
+var ime = "Isko";
+console.log(ime)
+ime =128;
+console.log (ime)
+//varijable su skladiste informacija
+
+//operatori su mnozenje deljenje sabiranje oduzimanje
+
+console.log(7 % 2); //ovo je modul koji oznacava ostatatak
+
+console.log("javasctript" + "je cudna")
