7a599455c47c604d79877c5e1ff41a3ebf7e5948
Edin Kucevic <edin@Edins-MacBook-Air.local>
2022-05-09 18:48:08 +0200

    operacije

---
+++ uvod/JavaScript/app.js
@@ -0,0 +1,89 @@
+
+//LET i CONST su nove varijable
+//const ne moze da se menja 
+//let moze da se menja
+
+//const ime = "Max"
+//nema promenu
+
+//let godine = 30;
+//console.log (ime +" "+ godine)
+
+//godine = 25;
+//console.log(ime + " " + godine)
+
+// console.log(5===5) kod provere 3 put jednako
+// console.log (5 !== 6) kod negacije 2 put jednako
+// console.log (6 < 50); koristi se za vece manje tj uporedjivanje
+
+// const ime = "Max";
+// const godine = 40;
+// const meseci = godine * 12;
+
+
+
+// console.log(meseci);
+
+// const ime = prompt("Unesi tvoje ime")
+// const godine = prompt ("Unesi tvoje godine")
+
+// alert("Ziveo si " + godine * 12 + "meseci")
+
+//prefixni i postfixni operatori
+//prefixni odradi odmah operaciju
+//postfixni odradi u sledecoj operaciji 
+
+// let age = 20;
+// console.log (++age)
+
+
+
+// KONDICIONALI
+
+// consst age = 17
+
+// if (age >= 18) {       //if je glavni deo
+//     console.log ("punoletan si")  //u zagradi je uslov , u viticastoj je nesto da se odradi ili blok
+// }
+// else if (age < 18) {       
+//     console.log ("nisi punoletan") //else je zadnji deo kondicionala
+// }
+// else {
+//      console.log ("Izvrsava s samo u slucaju kada ni jedan uslov nije zadovoljen")  
+
+
+// const age = prompt ("unesi svoje godine")
+
+// if (+age === 18) {
+//     console.log ("Tek si postao punoletan")
+// }
+// else if (age > 18) {
+//     console.log ("Ti si odrastao")
+// }
+
+// else if (age > 12) {
+//     console.log ("ti si tinejdzer")
+// }
+// else {
+//     console.log ("Ti si dete")
+// }
+
+//operatori
+// const a = 4; 
+// const b = 5;
+// const c = 0;
+//                            //ovo je AND operator
+// console.log(a && b && c); //da bi dobili true mora da su svaki true da bi ispunili && i vraca onaj poslednji u ovom slucaju C
+
+
+// const a = 4
+// const b = false
+
+// console.log ( a || b) //ovo je OR (ili) operator trazi da mu je jedan true i vraca na prvi u ovom slucaju A 
+
+// const a = 5
+// const b = 10
+// console.log (!5 || !false); // ( ! ) je negacija i pretvaranje true u false i obrnuto.
+
+
+
