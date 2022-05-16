e0c580cffd8fdbc3ba5fb2094338b84b9ba15347
Edin Kucevic <edin@Edins-MacBook-Air.local>
2022-05-16 18:40:51 +0200

    Switch statement

--- uvod/petlje/app.js
+++ uvod/petlje/app.js
@@ -34,3 +34,48 @@
 // } 
 // while (USLOV);
 
+//  for (let i = 0; i <100; i++) {
+//      if ( i % 3 === 0 && i % 5 === 0)
+//      console.log("FizzBuzz",i )
+//     else if (i % 3 === 0)
+//      console.log("Fizz", i);
+//     else if ( i % 5 == 0)
+//      console.log("Buzz",i);
+//      else {
+//          console.log(i);
+//     }
+// }
+
+
+// const rec = "Sta ste radili danas";
+// let counter = 0;
+
+// for(let i = 0; i < rec.length; i++) {
+//     if(rec[i] === " ") {
+//         counter++
+
+//     }
+// } console.log(counter++)
+
+// const string = 'ovo je string'
+// let newString = " " ;
+
+// for (let i = 0; i < string.length; i++) {
+    
+//     const element = string [i];
+
+//     if (i === string.length - 1) {
+//         newString += element.toUpperCase();
+
+//     } else if(string[ i + 1] === " ") {
+//         newString += element.toUpperCase();
+        
+     
+
+//     } else {newString += element};
+
+    
+
+    
+    
+// 
---
+++ uvod/switch statement/app.js
@@ -0,0 +1,42 @@
+// let godine= 15
+
+// switch (godine) {
+//     case 20: // : - pocetak bloka koda koji treba da se zavrsi
+//         console.log ("Ima 20 godina")
+//         break;
+//     case 15:
+//         console.log ("ima 15 godina")
+//         break;
+//     default:
+//         console.log ("nije upao ni u jedan case")
+// }
+
+       
+
+    
+//  if (isSunny) {
+//      console.log("Danas je suncano")
+//  } else {
+//      console.log ("Nije suncano")
+//  }
+
+//  if(isSunny) // if (isSunny === true)
+//  if (!isSunny) // if (isSunny !== true) || if (isSunny === false)
+
+
+
+          // TERNARY OPERATOR su skraceni oblik za else i if
+
+// 
+
+
+
+if ("") {
+    console.log ("TACNA VREDNOST ILI TRUTHY")
+
+} else {
+    console.log ("NETACNA VREDNOST ILI FALSY")
+}
+
+// true i false su tip podatka ili boolean
+// truthy i falsy su svi tipovi podataka
